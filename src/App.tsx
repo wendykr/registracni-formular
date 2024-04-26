7cca8ef263142e062e67b3669a11ce695f7eeaa5
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-26 12:25:17 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div className="container">
+      <header>
+        <div className="logo" />
+        <h1>Webová aplikace</h1>
+      </header>
+      <main>
+        <p>
+          Startovací šablona pro webovou aplikaci v Reactu.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
